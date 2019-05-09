367526d6457d9a8923ba95bad790a01215949d48
Stefan Scepanovic <scepa91@icloud.com>
2019-05-09 22:21:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,5 @@
-next_country = "Niger"
+
 countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
-countries_in_western_africa << "Niger"
+next_country = "Niger"
+countries_in_western_africa.push(next_country)
 end
